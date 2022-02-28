b252d7b0cb6e9eba6a4260e5dfdb62e8dcd00000
meatbot94 <meatbot94@gmail.com>
2022-02-28 14:47:50 +0900

    Added PopupWithForm

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,63 @@ import React from 'react';
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
+import PopupWithForm from './PopupWithForm';
 
 function App() {
+  // document.querySelector('#profileEditPopup').classList.add('popup_opened');
   return (
     <div className='body'>
       <Header />
       <Main />
       <Footer />
+
+      <PopupWithForm id='profileEditPopup' title='Редактировать профиль' formName='editForm' formId='editPopupForm' buttonText='Сохранить'>
+        <input
+          className='popup__input'
+          type='text'
+          name='nameInput'
+          id='nameInput'
+          placeholder='Имя'
+          minLength='2'
+          maxLength='40'
+          required
+        />
+        <span id='nameInput-err' className='popup__error'></span>
+        <input
+          className='popup__input'
+          type='text'
+          name='descriptionInput'
+          id='descriptionInput'
+          placeholder='Описание'
+          minLength='2'
+          maxLength='200'
+          required
+        />
+        <span id='descriptionInput-err' className='popup__error'></span>
+      </PopupWithForm>
+
+      <PopupWithForm id='cardAddPopup' title='Новое место' formName='addForm' formId='addPopupForm' buttonText='Создать'>
+        <input
+          className='popup__input'
+          type='text'
+          name='placeInput'
+          id='placeInput'
+          placeholder='Название'
+          minLength='2'
+          maxLength='30'
+          required
+        />
+        <span id='placeInput-err' className='popup__error'></span>
+        <input className='popup__input' type='url' name='linkInput' id='linkInput' placeholder='Ссылка на картинку' required />
+        <span id='linkInput-err' className='popup__error'></span>
+      </PopupWithForm>
+
+      <PopupWithForm id='updatePopup' title='Обновить аватар' formName='updateForm' formId='updatePopupForm' buttonText='Сохранить'>
+        <input className='popup__input' type='url' name='pictureInput' id='pictureInput' placeholder='Ссылка на картинку' required />
+        <span id='pictureInput-err' className='popup__error'></span>
+      </PopupWithForm>
+
+      <PopupWithForm id='confirmPopup' title='Вы уверены?' formName='confirmForm' formId='confirmPopupForm' buttonText='Да'></PopupWithForm>
     </div>
   );
 }
---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const PopupWithForm = props => {
+  return (
+    <div id={props.id} className={`popup`}>
+      <div className='popup__body'>
+        <button type='button' aria-label='Закрыть всплывающее окно' className='popup__close'></button>
+        <h2 className='popup__title'>{props.title}</h2>
+        <form action='#' method='post' name={props.formName} id={props.formId} className='popup__form'>
+          {props.children}
+          <button type='submit' class='popup__submit'>
+            {props.buttonText}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default PopupWithForm;
