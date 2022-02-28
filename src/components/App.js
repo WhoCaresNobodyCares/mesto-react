eeb5999a7db307f8b2078930c9148d9aef16fb95
meatbot94 <meatbot94@gmail.com>
2022-02-28 13:13:20 +0900

    Added Header, Main & Footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='body'>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
