dd140edd90212879374fa456dee67e1b9486a107
meatbot94 <meatbot94@gmail.com>
2022-03-14 19:14:06 +0900

    Added likes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import UserContext from '../contexts/CurrentUserContext';
 
 const Card = props => {
+  const currentUser = React.useContext(UserContext);
+
+  const isOwn = props.card._id === currentUser._id;
+  const cardDeleteButtonClassName = `card__remove ${isOwn ? `` : `card__remove_hidden`}`;
+
+  const isLiked = props.card.likes.some(item => item._id === currentUser._id);
+  const cardLikeButtonClassName = `card__like ${isLiked ? `card__like_active` : ``}`;
+
   return (
     <div className='card'>
-      <button type='button' aria-label='Удалить карточку' className='card__remove card__remove_hidden'></button>
+      <button type='button' aria-label='Удалить карточку' className={cardDeleteButtonClassName}></button>
       <img
         src={props.card.link}
         alt={props.card.name}
@@ -15,7 +24,14 @@ const Card = props => {
       <div className='card__description'>
         <h2 className='card__title'>{props.card.name}</h2>
         <div className='card__wrap'>
-          <button type='button' aria-label='Лайк' className='card__like'></button>
+          <button
+            type='button'
+            aria-label='Лайк'
+            className={cardLikeButtonClassName}
+            onClick={() => {
+              props.onCardLike(props.card);
+            }}
+          ></button>
           <span className='card__number'>{props.card.likes.length}</span>
         </div>
       </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import UserContext from '../contexts/CurrentUserContext';
 
 import api from '../utils/Api';
 import Card from './Card';
 
-const Main = props => {
-  const userContext = React.useContext(UserContext);
+import UserContext from '../contexts/CurrentUserContext';
 
-  // !!!
+const Main = props => {
+  const currentUser = React.useContext(UserContext);
 
   const [cards, setCards] = React.useState([]);
 
@@ -18,25 +17,34 @@ const Main = props => {
       .catch(error => console.log(`WASTED - ${error}`));
   }, []);
 
+  function handleCardLike(card) {
+    const isLiked = card.likes.some(item => item._id === currentUser._id);
+    if (!isLiked) {
+      api.putLike(card._id).then(likedCard => setCards(state => state.map(c => (c._id === card._id ? likedCard : c))));
+    } else {
+      api.removeLike(card._id).then(unlikedCard => setCards(state => state.map(c => (c._id === card._id ? unlikedCard : c))));
+    }
+  }
+
   return (
     <main className='main body__main'>
       <section id='profile' className='profile main__profile'>
         <div className='profile__shape'>
-          <div className='profile__avatar' style={{ backgroundImage: `url(${userContext.avatar})` }}></div>
+          <div className='profile__avatar' style={{ backgroundImage: `url(${currentUser.avatar})` }}></div>
           <div className='profile__overlay' onClick={props.onEditAvatar}></div>
         </div>
         <div className='profile__information'>
           <div className='profile__wrap'>
-            <h1 className='profile__username'>{userContext.name}</h1>
+            <h1 className='profile__username'>{currentUser.name}</h1>
             <button type='button' aria-label='Редактировать профиль' className='profile__edit' onClick={props.onEditProfile}></button>
           </div>
-          <p className='profile__description'>{userContext.about}</p>
+          <p className='profile__description'>{currentUser.about}</p>
         </div>
         <button type='button' aria-label='Добавить фото' className='profile__add' onClick={props.onAddPlace}></button>
       </section>
       <section id='elements' className='elements'>
         {cards.map(item => (
-          <Card card={item} key={item._id} onCardClick={props.onCardClick} />
+          <Card card={item} key={item._id} onCardClick={props.onCardClick} onCardLike={handleCardLike} />
         ))}
       </section>
     </main>
